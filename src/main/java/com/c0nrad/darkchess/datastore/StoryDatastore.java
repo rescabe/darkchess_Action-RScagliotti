bc26f2b06a24899536bc6b12a4bd8b015ca064a3
regina <r.scagliottib@gmail.com>
2025-06-29 20:52:23 -0300

    P StoryDatastore.java

--- src/main/java/com/c0nrad/darkchess/datastore/StoryDatastore.java
+++ src/main/java/com/c0nrad/darkchess/datastore/StoryDatastore.java
@@ -12,7 +12,7 @@ import org.bson.types.ObjectId;
 import xyz.morphia.Datastore;
 
 public class StoryDatastore {
-    private static final String KEY_P = "PPPPPPPP";
+    private static final String P = "PPPPPPPP";
     public static Story Find(String storyId) {
         Datastore d = MorphiaSingleton.GetDatastore();
         ObjectId objectId = new ObjectId(storyId);
@@ -43,7 +43,7 @@ public class StoryDatastore {
 
         String[] layout ={
             "    K   ", 
-            "KEY_P",
+            "P",
             "        ",
             "        ",
             "        ",
@@ -92,7 +92,7 @@ public class StoryDatastore {
 
         String[] layout3 ={
             "RNBQKBNR", 
-            "KEY_P",
+            "P",
             "        ",
             "        ",
             "        ",
@@ -116,8 +116,8 @@ public class StoryDatastore {
 
         String[] layout4 ={
             "PPPPKPPP", 
-            "KEY_P",
-            "KEY_P",
+            "P",
+            "P",
             "        ",
             "        ",
             "        ",
@@ -138,7 +138,7 @@ public class StoryDatastore {
 
         String[] layout5 ={
             "RNBQKBNR", 
-            "KEY_P",
+            "P",
             "        ",
             "        ",
             "        ",
